0b150459ee8205b5c6898e11aced1b6d3cfb9b51
shannonssd <shannonssd@gmail.com>
2025-04-10 11:39:10 +0800

    test-1

--- extractify_api/project/settings/base.py
+++ extractify_api/project/settings/base.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
-from distutils.util import strtobool
+    import os
+    from distutils.util import strtobool
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
